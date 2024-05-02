7b03d3f29a6923713636c3923e87b2e2fbe18597
mjz <2286101414@qq.com>
2024-05-02 10:31:00 +0800

    mjz update

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,131 @@
+const baiduCode = require('./config/baiduCode.js'); // 百度统计hm码
+const htmlModules = require('./config/htmlModules.js');
+
+module.exports = {
+
+  theme: 'vdoing', // 使用依赖包主题
+  // theme: require.resolve('../../vdoing'), // 使用本地主题
+
+  title: "MiAdmin",
+  description: '一个基于ThinkPHP6和React的极简后台管理系统',
+  base: '/mi-admin-docs/', // 默认'/'。如果你想将你的网站部署到如 https://foo.github.io/bar/，那么 base 应该被设置成 "/bar/",（否则页面将失去样式等文件）
+  head: [ // 注入到页面<head> 中的标签，格式[tagName, { attrName: attrValue }, innerHTML?]
+    ['link', { rel: 'icon', href: '/img/hero-image.png' }], //favicons，资源放在public文件夹
+    ['meta', { name: 'keywords', content: 'MiAdmin,react,antd,thinkphp' }],
+    ['meta', { name: 'theme-color', content: '#11a8cd' }], // 移动浏览器主题颜色
+  ],
+
+  // 主题配置
+  themeConfig: {
+    nav: [
+      { text: '首页', link: '/' },
+      {
+        text: '开始', link: '/pages/use/', items: [
+          { text: '准备', link: '/pages/use/' },
+          { text: '安装', link: '/pages/install/' },
+          { text: '目录', link: '/pages/catalogue/' },
+        ]
+      },
+      {
+        text: '指南', link: '/pages/router/', items: [
+          { text: '开发', link: '/pages/router/' },
+        ]
+      },
+      {
+        text: '教程', link: '/pages/install-phpStudy/', items: [
+          { text: '安装后端', link: '/pages/install-phpStudy/' },
+          { text: 'hosts配置', link: '/pages/install-hosts/' },
+          { text: 'phpStudy中PHP配置', link: '/pages/install-php/' },
+        ]
+      },
+      { text: '支持', link: '/pages/support/' },
+    ],
+    sidebarDepth: 1, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
+    logo: '/img/hero-image.png', // 导航栏logo
+    repo: 'https://gitee.com/miren123/mi-admin-react', // 导航栏右侧生成Github链接
+    searchMaxSuggestions: 10, // 搜索结果显示最大数
+    lastUpdated: '上次更新', // 更新的时间，及前缀文字 string | boolean (取值为git提交时间)
+
+    // 以下配置是Vdoing主题改动的和新增的配置
+    sidebar: { mode: 'structuring', collapsable: false }, // 侧边栏  'structuring' | { mode: 'structuring', collapsable: Boolean} | 'auto' | 自定义    温馨提示：目录页数据依赖于结构化的侧边栏数据，如果你不设置为'structuring',将无法使用目录页
+
+    // sidebarOpen: false, // 初始状态是否打开侧边栏，默认true
+    updateBar: { // 最近更新栏
+      showToArticle: false, // 显示到文章页底部，默认true
+      // moreArticle: '/archives' // “更多文章”跳转的页面，默认'/archives'
+    },
+
+    // categoryText: '随笔', // 碎片化文章（_posts文件夹的文章）预设生成的分类值，默认'随笔'
+
+    // contentBgStyle: 1,
+
+    category: false, // 是否打开分类功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含分类字段 2.页面中显示与分类相关的信息和模块 3.自动生成分类页面（在@pages文件夹）。如关闭，则反之。
+
+    tag: false, // 是否打开标签功能，默认true。 如打开，会做的事情有：1. 自动生成的frontmatter包含标签字段 2.页面中显示与标签相关的信息和模块 3.自动生成标签页面（在@pages文件夹）。如关闭，则反之。
+    // archive: false, // 是否打开归档功能，默认true。 如打开，会做的事情有：1.自动生成归档页面（在@pages文件夹）。如关闭，则反之。
+
+    author: { // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, href: String}
+      name: 'miren', // 必需
+      href: 'https://gitee.com/miren123' // 可选的
+    },
+    social: { // 社交图标，显示于博主信息栏和页脚栏
+      // iconfontCssFile: '//at.alicdn.com/t/font_1678482_u4nrnp8xp6g.css', // 可选，阿里图标库在线css文件地址，对于主题没有的图标可自由添加
+      icons: [
+        {
+          iconClass: 'icon-gitee',
+          title: 'Gitee',
+          link: 'https://gitee.com/miren123'
+        },
+        // {
+        //   iconClass: 'icon-github',
+        //   title: 'GitHub',
+        //   link: 'https://github.com/miren521'
+        // }
+      ]
+    },
+    footer: { // 页脚信息
+      createYear: 2022, // 创建年份
+      copyrightInfo: 'miren | MIT License', // 版权信息，支持a标签
+    },
+    htmlModules,
+  },
+
+  // 插件
+  plugins: [
+    ['fulltext-search'], // 全文搜索
+
+    [
+      'vuepress-plugin-baidu-tongji', // 百度统计
+      {
+        hm: baiduCode
+      }
+    ],
+
+    ['one-click-copy', { // 代码块复制按钮
+      copySelector: ['div[class*="language-"] pre', 'div[class*="aside-code"] aside'], // String or Array
+      copyMessage: '复制成功', // default is 'Copy successfully and then paste it for use.'
+      duration: 1000, // prompt message display time.
+      showInMobile: false // whether to display on the mobile side, default: false.
+    }],
+
+    [
+      'vuepress-plugin-zooming', // 放大图片
+      {
+        selector: '.theme-vdoing-content img:not(.no-zoom)',
+        options: {
+          bgColor: 'rgba(0,0,0,0.6)'
+        },
+      },
+    ],
+
+    [
+      '@vuepress/last-updated', // "上次更新"时间格式
+      {
+        transformer: (timestamp, lang) => {
+          const dayjs = require('dayjs') // https://day.js.org/
+          return dayjs(timestamp).format('YYYY/MM/DD, HH:mm:ss')
+        },
+      }
+    ]
+  ],
+}
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+home: true
+heroImage: /img/hero-image.png
+heroText: MiAdmin
+tagline: 基于ThinkPHP6和React的极简后台管理系统
+actionText: 开始使用 →
+actionLink: /pages/use/
+bannerBg: none # auto => 网格纹背景(有bodyBgImg时无背景)，默认 | none => 无 | '大图地址' | background: 自定义背景样式 提示：可以到palette.style修改背景颜色$bannerTextColor变量
+
+features: # 可选的
+  - title: 前后分离
+    details: 前后分离开发模式，开发更清晰，分工更明确，提升开发效率，API、JSON数据交互，JWT用户认证。
+  - title: 简单轻量
+    details: 快速、简单、轻量，保持出色的性能和至简代码的同时，更注重易用性，代码一目了然，上手简单。
+  - title: 免费开源
+    details: Apache2开源协议，无需授权即可商业化使用，代码全部免费开源，没有任何加密。
+  - title: 开箱即用
+    details: 登录退出、权限管理、日志管理、接口文档、动态路由等基本功能，前后台基础框架，安装后即可使用。
+  - title: 接口文档
+    details: 编写注释即可自动生成API文档，支持公共注释定义、业务逻辑层、数据表字段的引用，在线调试。
+  - title: 内容管理
+    details: 文章管理、分类管理、设置管理、动态路由、权限管理、用户管理、角色管理、个人设置。
+
+---
